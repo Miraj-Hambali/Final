e0c14a9885d0e1a8a57634036a915c70d8397916
hambalimiraj <hambalimiraj@gmail.com>
2020-10-21 21:17:02 +0800

    Begin

---
+++ resources/views/blog/form.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+
+
+
+@section('content')
+    
+<form>
+    <div class="form-group">
+      <label for="exampleFormControlInput1">Judul</label>
+      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="form-group">
+      <label for="exampleFormControlTextarea1">Example textarea</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+  </form>
+
+@endsection
